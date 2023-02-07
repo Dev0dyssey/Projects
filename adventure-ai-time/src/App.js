21ddfe1410daf95fc0e645aac8278bbc57db3619
Thomas Andrew Martin <47917727+Dev0dyssey@users.noreply.github.com>
2023-02-07 22:36:20 +0000

    Added option buttons and further story generation
    Additional API call made to continue text generation, and dynamically created buttons added to the code

--- adventure-ai-time/src/App.js
+++ adventure-ai-time/src/App.js
@@ -7,29 +7,50 @@ function App() {
   const apiKey = process.env.REACT_APP_OPENAI_KEY;
   const [genrePrompt, setGenrePrompt] = useState("");
   const [story, setStory] = useState("Your journey is about to begin");
+  const [options, setOptions] = useState([])
   const configuration = new Configuration({
     apiKey: apiKey,
   });
 
   const openai = new OpenAIApi(configuration);
 
+  const getOptions = (story) => {
+    const options = story.split("Option");
+    const optionsArray = options.slice(1).map((option) => option.replace(/\d+: /, '').trim());
+    console.log("Options: ", optionsArray);
+    setOptions(optionsArray);
+    return optionsArray;
+  };
+
+  const choiceButtons = (optionChoices) => {
+    return (
+      <div>
+        {optionChoices.map((item, index) => (
+          <button key={index} onClick={() => goOnAdventure(story)}>{item}</button>
+        ))}
+      </div>
+    )
+  }
+
   const startAdventure = async () => {
     const start = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `Start a adventure in a ${genrePrompt} setting. Give background of the world and explain how the adventurer found themselves at the crossroad. Generate two options of what the adventurer can do`,
+      prompt: `Start a adventure in a ${genrePrompt} setting. Give background of the world and explain how the adventurer found themselves at the crossroad. At least three paragraphs for the description. Present two options, marked as Option 1 Option 2 in this format. Each Option should be no more than five words. Ideally a simple one word or binary options. Always include the options at the end of the generated text.`,
       n: 1,
       max_tokens: 4000,
     });
-    console.log(start);
-    setStory(start.data.choices[0].text);
+    let story = start.data.choices[0].text
+    console.log(story);
+    setStory(story);
+    getOptions(story);
   };
 
   const goOnAdventure = async (prompt) => {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `Adventurer standing at a crossroad, and looks ${prompt}. Describe what they are seeing`,
-      n: 2,
-      max_tokens: 4000,
+      prompt: `${prompt}. continue the story based on Option 1 with a minimum of two paragraphs. Include two options at end of text in the format Option 1 and Option 2`,
+      n: 1,
+      max_tokens: 3700,
     });
     console.log(response);
   };
@@ -74,11 +95,12 @@ function App() {
         <div>
           <button onClick={startAdventure}>BEGIN ADVENTURE</button>
         </div>
-        <div>{TextWithLineBreaks(story)}</div>
-        <div>
-          <button onClick={() => goOnAdventure("left")}>Left</button>
-          <button onClick={() => goOnAdventure("right")}>Right</button>
-        </div>
+        <div>{TextWithLineBreaks(story)}</div> 
+        {options.length > 0 && (
+          <div>
+            {choiceButtons(options)}
+          </div>
+        )}   
       </header>
     </div>
   );

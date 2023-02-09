bc7b217d51e3161059b2135b0e1e7a4b514fed3a
Thomas Andrew Martin <47917727+Dev0dyssey@users.noreply.github.com>
2023-02-09 12:25:01 +0000

    Refactored handling of responses
    Refactored the handling OpenAI API response to dynamically set options and remove options being inside the text shown to the user. Updated second API call to pass in whichever option the user chose

--- adventure-ai-time/src/App.js
+++ adventure-ai-time/src/App.js
@@ -7,52 +7,50 @@ function App() {
   const apiKey = process.env.REACT_APP_OPENAI_KEY;
   const [genrePrompt, setGenrePrompt] = useState("");
   const [story, setStory] = useState("Your journey is about to begin");
-  const [options, setOptions] = useState([])
+  const [options, setOptions] = useState([]);
   const configuration = new Configuration({
     apiKey: apiKey,
   });
 
   const openai = new OpenAIApi(configuration);
 
-  const getOptions = (story) => {
-    const options = story.split("Option");
-    const optionsArray = options.slice(1).map((option) => option.replace(/\d+: /, '').trim());
-    console.log("Options: ", optionsArray);
-    setOptions(optionsArray);
-    return optionsArray;
-  };
-
-  const choiceButtons = (optionChoices) => {
-    return (
-      <div>
-        {optionChoices.map((item, index) => (
-          <button key={index} onClick={() => goOnAdventure(story)}>{item}</button>
-        ))}
-      </div>
-    )
-  }
-
   const startAdventure = async () => {
-    const start = await openai.createCompletion({
+    const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `Start a adventure in a ${genrePrompt} setting. Give background of the world and explain how the adventurer found themselves at the crossroad. At least three paragraphs for the description. Present two options, marked as Option 1 Option 2 in this format. Each Option should be no more than five words. Ideally a simple one word or binary options. Always include the options at the end of the generated text.`,
       n: 1,
-      max_tokens: 4000,
+      max_tokens: 2048,
     });
-    let story = start.data.choices[0].text
-    console.log(story);
-    setStory(story);
-    getOptions(story);
+    let storyText = response.data.choices[0].text.split("Option");
+    let storyResponse = response.data.choices[0].text;
+    setStory(storyText[0]);
+    getOptions(storyResponse);
   };
 
-  const goOnAdventure = async (prompt) => {
+  const updateAdventure = (newStory) => {
+    setStory((story) => story + newStory);
+  };
+
+  const continueAdventure = async (prompt, option) => {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${prompt}. continue the story based on Option 1 with a minimum of two paragraphs. Include two options at end of text in the format Option 1 and Option 2`,
+      prompt: `${prompt}. continue the story based on ${option} with the new story being a minimum of two paragraphs. Include two options at end of text in the format Option 1 and Option 2, each option should be no more than three words`,
       n: 1,
-      max_tokens: 3700,
+      max_tokens: 2048,
     });
-    console.log(response);
+    let storyText = response.data.choices[0].text.split("Option");
+    let storyResponse = response.data.choices[0].text;
+    updateAdventure(storyText[0]);
+    getOptions(storyResponse);
+  };
+
+  const getOptions = (story) => {
+    const options = story.split("Option");
+    const optionsArray = options
+      .slice(1)
+      .map((option) => option.replace(/\d+: /, "").trim());
+    setOptions(optionsArray);
+    return optionsArray;
   };
 
   const TextWithLineBreaks = (story) => {
@@ -76,6 +74,18 @@ function App() {
     );
   };
 
+  const choiceButtons = (optionChoices) => {
+    return (
+      <div>
+        {optionChoices.map((item, index) => (
+          <button key={index} onClick={() => continueAdventure(story, item)}>
+            {item}
+          </button>
+        ))}
+      </div>
+    );
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -95,12 +105,8 @@ function App() {
         <div>
           <button onClick={startAdventure}>BEGIN ADVENTURE</button>
         </div>
-        <div>{TextWithLineBreaks(story)}</div> 
-        {options.length > 0 && (
-          <div>
-            {choiceButtons(options)}
-          </div>
-        )}   
+        <div>{TextWithLineBreaks(story)}</div>
+        {options.length > 0 && <div>{choiceButtons(options)}</div>}
       </header>
     </div>
   );
